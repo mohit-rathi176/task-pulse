05546f222cee49ba27b895c2bf9c91525e628043
mohit-rathi176 <mohitrathi176@gmail.com>
2025-04-30 10:24:19 +0530

    Add services in DI

--- TaskPulse/Program.cs
+++ TaskPulse/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using TaskPulse.Repository;
+using TaskPulse.Repository.Interfaces;
+using TaskPulse.Repository.Repositories;
+using TaskPulse.Service.Interfaces;
+using TaskPulse.Service.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+#region Add services to the container
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -12,11 +15,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString"), sqlOptions => sqlOptions.MigrationsAssembly("TaskPulse.Repository"));
     options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
 });
+#endregion
+
+#region DI
+builder.Services.AddScoped<ITaskRepository, TaskRepository>();
+builder.Services.AddScoped<ITaskService, TaskService>();
+#endregion
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+#region Configure the HTTP request pipeline
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
@@ -24,3 +32,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+#endregion
